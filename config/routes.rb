4a6e05d8c6b24fd6e6444b68e7299442f83312cf
Dmitry Gusev <dmitry@leviossa.net>
2020-03-08 20:51:11 +0300

    Updated routes and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :questions
   devise_for :companies
-  root to: 'members#index'
+  root to: 'member_tests#index'
 
   resources :tests
   resources :members
