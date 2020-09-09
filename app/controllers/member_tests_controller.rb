d81e66792e0aaa61819a00c3da9959af3959688b
Grigory Bogush <g.bogush@leviossa.net>
2020-09-09 17:09:38 +0300

    used statuses array directly

--- app/controllers/member_tests_controller.rb
+++ app/controllers/member_tests_controller.rb
@@ -3,8 +3,7 @@ class MemberTestsController < ApplicationController
   before_action :validate_regeneration, only: [:regenerate]
 
   def index
-    @statuses = MemberTest.statuses.keys
-    @status_filter = @statuses.include?(params[:status]) ? params[:status] : 'passed'
+    @status_filter = MemberTest.statuses.keys.include?(params[:status]) ? params[:status] : 'passed'
     @member_tests = current_company.member_tests.where(status: @status_filter)
   end
 
