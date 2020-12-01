12727d10b7ca3b37c63ab26c7951fd4b616616ba
Grigory Bogush <g.bogush@leviossa.net>
2020-12-01 15:46:24 +0300

    added matrix on member group show page

--- app/models/member_group.rb
+++ app/models/member_group.rb
@@ -4,6 +4,7 @@ class MemberGroup < ApplicationRecord
   has_many :members, dependent: :nullify
   has_many :member_group_test_groups, dependent: :destroy
   has_many :test_groups, through: :member_group_test_groups
+  has_many :tests, through: :test_groups
 
   validates :name, uniqueness: { scope: :company_id, message: 'Duplicate member groups not allowed' }
 end
