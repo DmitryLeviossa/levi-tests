e4095d7251c4cc255d4db27c1153acf4c953692c
Dmitriy Kalganov <dmitriu355@gmail.com>
2022-06-08 16:05:59 +0300

    correct API::AplicationController

--- app/controllers/API/application_controller.rb
+++ app/controllers/API/application_controller.rb
@@ -1,3 +1,5 @@
-class API::ApplicationController < ActionController::API
-  acts_as_token_authentication_handler_for Company
+module API
+  class ApplicationController < ActionController::API
+    acts_as_token_authentication_handler_for Company
+  end
 end
